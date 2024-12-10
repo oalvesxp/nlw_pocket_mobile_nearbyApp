02af08c8923d95187bd7771e240fcf7acd1325b9
Osvaldo Alves <oalves.neto@outlook.com>
2024-12-10 11:52:10 -0300

    feat: use welcome component on home page

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,13 +1,11 @@
 import { View, Text } from 'react-native'
 
+import { Welcome } from '@/components/welcome'
+
 export default function Index() {
   return (
-    <View style={{
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center'
-    }}>
-      <Text style={{ fontSize: 22 }}>Hello React Native!</Text>
+    <View style={{ flex: 1, padding: 40, gap: 40 }}>
+      <Welcome />
     </View>
   )
 }

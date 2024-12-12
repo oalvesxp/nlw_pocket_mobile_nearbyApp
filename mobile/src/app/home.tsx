a8b5660377d381743504745d16b6e4a0523fcec3
Osvaldo Alves <oalves.neto@outlook.com>
2024-12-12 09:37:53 -0300

    feat: add function to get currentLocation from user cellphone

--- mobile/src/app/home.tsx
+++ mobile/src/app/home.tsx
@@ -1,13 +1,18 @@
 import { useEffect, useState } from 'react'
 import { View, Alert } from 'react-native'
+import MapView from 'react-native-maps'
+import * as Location from 'expo-location'
 
 import { api } from '@/services/api'
 import { Categories, CategoriesProps } from '@/components/categories'
 import { PlaceProps } from '@/components/place'
 import { Places } from '@/components/places'
 
-type MarketsProps = PlaceProps & {
+type MarketsProps = PlaceProps
 
+const currentLocation = {
+  latitude: -23.561187293883442,
+  longitude: -46.656451388116494
 }
 
 export default function Home() {
@@ -40,6 +45,19 @@ export default function Home() {
     }
   }
 
+  async function getCurrentLocation() {
+    try {
+      const { granted } = await Location.requestForegroundPermissionsAsync()
+
+      if (granted) {
+        const location = await Location.getCurrentPositionAsync()
+        console.log(location)
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   useEffect(() => {
     fetchCategories()
   }, [])
@@ -48,12 +66,26 @@ export default function Home() {
     fetchMarkets()
   }, [category])
 
+  useEffect(() => {
+    getCurrentLocation()
+  }, [])
+
   return (
     <View style={{ flex: 1, backgroundColor: '#CECECE ' }}>
       <Categories
         data={categories}
         onSelect={setCategory}
         selected={category}
+      />
+
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: currentLocation.latitude,
+          longitude: currentLocation.longitude,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        }}
       />
 
       <Places data={markets} />
